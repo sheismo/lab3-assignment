bdde022644abec94e7adf9e82858eeb16609d770
Zainab <ajumobizainab@gmail.com>
2022-04-15 11:23:27 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab3Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string userInput;
+            Console.Write("Enter a number:");
+            userInput = Console.ReadLine();
+            int number = Convert.ToInt32(userInput);
+            
+            if (number == 0 || number == 1)
+            {
+                Console.WriteLine("not prime number");
+            }
+            else
+            {
+                for(int i = 2; i <= number / 2; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        Console.WriteLine("not a prime number");
+                        return;
+                    }
+                }
+                Console.WriteLine("prime number");
+            }
+        }
+    }
+}
